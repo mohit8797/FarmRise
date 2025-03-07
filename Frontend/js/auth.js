d757899a02b5fc652ff41a2f15da38c5763556c6
mohit8797 <148079174+mohit8797@users.noreply.github.com>
2025-03-08 00:55:48 +0530

    Delete Frontend directory

--- Frontend/js/auth.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const token = localStorage.getItem('token');
-  
-    if (!token) {
-      window.location.href = 'login.html';
-    }
-  });
